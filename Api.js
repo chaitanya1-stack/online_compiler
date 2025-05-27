f652d290d44c530f1b4fb9cd15ea2f9ade21f38e
chaitanya1-stack <ck758779@gmail.com>
2025-05-27 16:19:40 +0530

    Use dynamic PORT for Render.

--- Api.js
+++ Api.js
@@ -111,6 +111,8 @@ app.post("/compile", (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
